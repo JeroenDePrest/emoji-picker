996471a371d30a3242814755924d96f8e02964b9
Jeroen De Prest <jeroen.deprest@hotmail.com>
2023-02-26 16:29:49 +0100

    better text align

--- src/App.tsx
+++ src/App.tsx
@@ -68,6 +68,7 @@ function App() {
             <Typography
               variant="h2"
               fontSize="2rem"
+              textAlign="center"
             >{`:${emojiName}:`}</Typography>
           </Box>
         </Grid>
